9a55cf7c55239b56c01cdf3a8e468b40cc6f019d
tlarbals824 <simkm0824@naver.com>
2022-07-04 00:00:08 +0900

    mysql 설정 및 일부 도메인 오류 발생에 따라 수정

--- src/main/java/webproject/ticket/domain/Category1.java
+++
@@ -1,6 +0,0 @@
-package webproject.ticket.domain;
-
-public enum Category1 {
-
-    CONCERT, MUSICAL, SPORTS
-}
--- src/main/java/webproject/ticket/domain/member/Member.java
+++ src/main/java/webproject/ticket/domain/member/Member.java
@@ -3,11 +3,16 @@ package webproject.ticket.domain.member;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Getter
+@Entity
 @RequiredArgsConstructor
 public class Member {
 
-    private final String member_id;
+    @Id
+    private String member_id;
     private String member_name;
     private String pw;
     private Grade grade;
