10f250cf45a47aaf6b60d0985828162f9e7b060d
hyunmin <joyjhm@naver.com>
2022-07-06 15:33:04 +0900

    place, show 수정

--- src/main/java/webproject/ticket/domain/show/Place.java
+++ src/main/java/webproject/ticket/domain/show/Place.java
@@ -1,20 +1,24 @@
-package webproject.ticket.domain;
+package webproject.ticket.domain.show;
 
 
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
 @Getter @Setter
 public class Place {
 
-    @Id
+    @Id @GeneratedValue
+    @Column(name="place_id")
     private Long place_id;
 
-    private String location;
-    private Seat seat;
+    private String location;    //장소
+    private String seat;        //좌석 번호
+
 }
 
